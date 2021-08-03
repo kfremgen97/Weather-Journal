0557f0635c780037d763d22592dabb5849a187ed
Kevin Fremgen <kevin.a.fremgen@gmail.com>
2021-08-03 11:27:05 -0400

    feat: Set up express server

---
+++ server/src/app.js
@@ -0,0 +1,30 @@
+// Imports
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const config = require('./config');
+
+// Initialize express app
+const app = express();
+
+// Add body-parser middle ware
+// Set up middle ware that only parses urlencoded bodies and
+// only looks at requests where the Content-Type header matches the type option.
+app.use(bodyParser.urlencoded({ extended: false }));
+// Set up middle ware that only parses json and looks at request with Content-Type to be json
+app.use(bodyParser.json());
+
+// Add cors middle ware
+// Allow cross origin sharing
+app.use(cors());
+
+// Simple get
+app.get('/', (req, res) => {
+    res.send('Welcome to the express server');
+});
+
+// Start the server
+app.listen(config.server.PORT, () => {
+    console.log('Starting express server');
+    console.log(`Listening on localhost: ${config.server.PORT}`);
+});
